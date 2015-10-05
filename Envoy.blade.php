465cc37abd79ec0872c9d8a030576806d14eaf78
Boyer <julien@meetserious.com>
2015-10-05 15:41:05 +0200

    add changes

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -10,8 +10,8 @@
     composer install -n --no-dev --no-scripts
     php artisan migrate
     mkdir storage
-    chmod 755 -R *
-    chmod 777 -R storage
+    chmod 777 -R *
+    sudo chown -R www-data:www-data /var/www/html/laracinema
     mkdir storage/framework storage/app storage/logs
     php artisan cache:clear
     ls -la
